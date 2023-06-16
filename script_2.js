a12604675ecac3c65502ffd1032f4956572bf8a7
HamideT4 <hamidemahamat20@gmail.com>
2023-06-16 16:37:34 +0100

    script de permettant de calculer la factorielle d'un nombre entré par l'utilisateur

---
+++ script_2.js
@@ -0,0 +1,12 @@
+number = prompt("De quel nombre veux-tu calculer la factorielle ?");
+
+factorial = 1
+
+for(let index = 1; index <= number; index++){
+
+    factorial = factorial*index
+
+} 
+
+console.log(`Le resultat est : ${factorial}`)
+
