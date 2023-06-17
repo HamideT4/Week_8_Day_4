2d292f9e514b158c42fc9b928f158341100ee847
HamideT4 <hamidemahamat20@gmail.com>
2023-06-17 09:18:38 +0100

    ajout de commentaires

--- script_2.js
+++ script_2.js
@@ -1,12 +1,16 @@
+// On recupère le nombre dont on veut calculer la factorielle
 number = prompt("De quel nombre veux-tu calculer la factorielle ?");
 
+// On initialise la factorielle à 1
 factorial = 1
 
 for(let index = 1; index <= number; index++){
 
+    //On calcule la factorielle par récursivité
     factorial = factorial*index
 
 } 
 
+// On affiche le résultat
 console.log(`Le resultat est : ${factorial}`)
 
